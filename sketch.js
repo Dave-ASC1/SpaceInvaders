fcf1be64411b546f53dcd336e1e337c11595787c
Dave-ASC1 <ademoyedavid11@gmail.com>
2019-07-19 16:01:03 -0400

    SpaceInvaders

---
+++ sketch.js
@@ -0,0 +1,76 @@
+/*
+Pseudo code:
+
+Main goals:
+1. Create a canvas and a black background.
+
+2. Create a ship at the bottom of the screen.
+
+3. Allow that ship to move to the left or right when the user clicks on the approprate keyboard button.
+
+4. When the user clicks on the space bar, allow for a projectile to shoot from the ship upward.
+
+5. Create an enemy that can be destroyed when the projectile hits it.
+
+6. Display a win screen if enemy has been destroyed.
+
+Extra credit:
+1. Allow the enemy to move in a pattern similar to traditional space invaders.
+2. Allow the enemy to be able to hurt the player when touched (one hit kills are ok)
+3. Create a way to reset the board
+4. Track the users score
+  - Number of hits
+  - Number of ships remaining
+5. Randomize enemy speed.
+
+*/
+var shipX;
+var shipImage;
+var spaceBackground;
+var laserX;
+var laserY;
+var aliens;
+
+function preload() {
+  shipImage = loadImage('images/sonic.png');
+  spaceBackground = loadImage('images/space background.jpg')
+  aliens = loadImage('images/JetpackAlien.png')
+}
+
+function setup() {
+  createCanvas(500, 500);
+  shipX = width / 2 - 25;
+  laserY = height - 100;
+  laserX = shipX + 65;
+}
+
+function draw() {
+  background("black");
+  image(spaceBackground, 0, 0);
+  circle(laserX, laserY, 10);
+
+  // rect(shipX, height - 100, 50, 100);
+  image(shipImage, shipX, height - 100);
+  
+  
+    if (keyIsDown(RIGHT_ARROW)) {
+      shipX += 10;
+      laserX = shipX + 65;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+      shipX -= 10;
+      laserX = shipX + 65;
+      console.log(shipX);
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      y += 5;
+    
+    if (keyIsDown(UP_ARROW)) {
+        y -= 5;
+    }
+    if (keyCode == 32) {
+      
+      laserY -= 10;
+    }
+  }
+}
